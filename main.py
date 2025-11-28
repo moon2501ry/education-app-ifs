110ce12d24c658e9319a1b09813624384897c934
Ryan A. O. <159044164+moon2501ry@users.noreply.github.com>
2025-11-28 17:37:53 +0000

    Final

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from pycli import *
 from config import ConfigTXT
+import time
 
 def init():
     config = ConfigTXT();
 
 def main():
     _input = input();
+    params = get_params(_input);
     match get_command(_input):
-        case connect:
-            params = get_params(_input);
+        case "init_math":
+
+
+
 
 if __name__ == "__main__":
+    init();
     while True:
         main();
