53bc6cf1e068d14ea2cc527b41ba3c4ff98ebac8
Ryan A. O. <159044164+moon2501ry@users.noreply.github.com>
2025-12-07 18:55:14 -0300

    Game Add

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import random as rnd
 import time
 
 def init():
-    config = ConfigTXT();
+    print("Iniciado");
 
 def main():
     _input = input();
@@ -12,17 +12,22 @@ def main():
     match get_command(_input):
         case "init_math":
             input("O jogo foi começou. Pressione Enter para continuar...");
+            run = True
+            while run:
+                clear();
+                operation = rnd.choice(["+","-"]);
+                number_1 = rnd.randint(0,100);
+                number_2 = rnd.randint(0,100);
+                result = (number_1 + number_2) if operation == "+" else (number_1 - number_2);
+                print(str(number_1)+""+operation+""+str(number_2));
+                result_user = input("Qual a Resposta? ");
+                if int(result_user) == result:
+                    input("Você ganhou!!! Pressione Enter para continuar...");
+                    # run = False;
+                elif int(result_user) != result:
+                    print(result,result_user);
+                    input("Tente dnv!! Pressione Enter para continuar...");
             clear();
-            operation = rnd.choice(["+","-"]);
-            number_1 = rnd.randint(0,100);
-            number_2 = rnd.randint(0,100);
-            result = (number_1 + number_2) if operation == "+" else (number_1 - number_2);
-            result_user = input("")
-            print();
-            
-
-
-
 
 if __name__ == "__main__":
     init();
