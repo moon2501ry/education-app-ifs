9ad3ebaec4521058d923a46518f5abcdcc1b0594
Ryan A. O. <159044164+moon2501ry@users.noreply.github.com>
2025-12-15 19:24:38 -0300

    Aprimora a lógica de conexão e gerenciamento de jogadores, atualiza a estrutura de requisições para o servidor e implementa feedbacks de status.

--- api.py
+++ api.py
@@ -28,12 +28,11 @@ class Servers:
         number_1 = rnd.randint(0,100);
         number_2 = rnd.randint(0,100);
         result = (number_1 + number_2) if operation == "+" else (number_1 - number_2);
-        self.o_keys[key]["Equation"] = [operation,number_1,number_2,result];
-        return self.o_keys[key]["Equation"];
+        self.o_keys[key]["Rounds"].append({"Operation":operation,"Numbers":[number_1,number_2],"Result":result,"Winner":0});
+        return self.o_keys[key]["Rounds"][-1];
     def connect_player(self,key:str,name:str):
         self.o_keys[key]["Players"]["Nmb"] += 1;
         self.o_keys[key]["Players"]["Users"][name] = {
-            "Op":False,
             "Points":0
         };
     def get(self,key:str,_var:str):
@@ -47,35 +46,54 @@ def add_or_go(o_key:str,maxplayers:int,name:str):
     servers.go_server(o_key);
     servers.connect_player(o_key,name);
 
-@app.put("/{o_key}/ready")
-def update_equation(o_key:str):
-    # Terminar
-    servers.update_equation(o_key);
-
-@app.put("/{o_key}/result/{user}/{result}")
-def note_result(o_key:str,user:str,result:int):
-    if result == servers[o_key]["Equation"][-1]:
-        servers[o_key]["User"]
-        # Terminar
-
-@app.get("/{o_key}/get_equation")
-async def get_equation(o_key:str):
-    return {"Equation":servers.get(o_key,"Equation")};
-
-@app.get("/{o_key}/players")
-async def get_players(o_key:str):
-    return {"Players":servers.get(o_key,"Players"),"NmbPlayers":servers.get(o_key,"NmbPlayers"),"MaxPlayer":servers.get(o_key,"MaxPlayer")};
-
-@app.get("/{o_key}/connect/{name}")
-async def online(o_key:str,name:str):
+@app.put("/{o_key}/connect/{name}")
+def online(o_key:str,name:str):
+    # Conecta os jogadores
     if servers.get(o_key,"Online") == False:
         return {"Status":"Offline"};
     elif servers.get(o_key,"Online"):
-        if servers.get(o_key,"NmbPlayers") < servers.get(o_key,"MaxPlayer"):
+        if servers.o_keys[o_key]["Players"]["Nmb"] < servers.o_keys[o_key]["Players"]["Max"]:
             servers.connect_player(o_key,name);
             return {"Status":"Connected"};
         else:
             return {"Status":"NoConnected"};
+
+@app.put("/{o_key}/ready")
+def update_equation(o_key:str):
+    # Retorna se foi dado o Ready
+    ready = False;
+    servers.o_keys[o_key]["Players"]["Ready"] += 1;
+    if servers.o_keys[o_key]["Players"]["Ready"] >= servers.o_keys[o_key]["Players"]["Nmb"]:
+        servers.update_equation(o_key);
+        ready = True;
+    return {"Ready":ready};
+
+@app.put("/{o_key}/result/{user}/{result}")
+def note_result(o_key:str,user:str,result:int):
+    # Retorna 0 caso não tenha conseguido acertar ou o nome do jogador
+    if servers.o_keys[o_key]["Players"]["Ready"] != 0: servers.o_keys[o_key]["Players"]["Ready"] = 0;
+    if result == servers.o_keys[o_key]["Rounds"][-1]["Result"] and servers.o_keys[o_key]["Rounds"][-1]["Winner"] == 0:
+        servers.o_keys[o_key]["Rounds"][-1]["Winner"] = user;
+        servers.o_keys[o_key]["Players"]["Users"][user]["Points"] += 1;
+    return {"Winner":servers.o_keys[o_key]["Rounds"][-1]["Winner"]};
+
+@app.get("/{o_key}/get_ready")
+async def get_ready(o_key:str):
+    # Retorna se foi dado o Ready
+    ready = False;
+    if servers.o_keys[o_key]["Players"]["Ready"] >= servers.o_keys[o_key]["Players"]["Nmb"]:
+        ready = True;
+    return {"Ready":ready,"Readys":servers.o_keys[o_key]["Players"]["Ready"]};
+
+@app.get("/{o_key}/get_equation")
+async def get_equation(o_key:str):
+    # Retorna a equação
+    return {"Equation":servers.o_keys[o_key]["Rounds"][-1]};
+
+@app.get("/{o_key}/players")
+async def get_players(o_key:str):
+    # Retorna tudo sobre os players
+    return {"Names":list(servers.o_keys[o_key]["Players"]["Users"].keys()),"Nmb":servers.o_keys[o_key]["Players"]["Nmb"],"Max":servers.o_keys[o_key]["Players"]["Max"]};
 
 @app.get("/{o_key}")
 async def main(o_key:str):
