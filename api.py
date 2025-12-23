41769492489a969a018e1b17de5c41f5b07fdd60
Ryan A. O. <159044164+moon2501ry@users.noreply.github.com>
2025-12-23 18:08:54 -0300

    Aprimora a lógica de gerenciamento de partidas, adiciona novos endpoints para conexão e desconexão de jogadores, e atualiza a interação do usuário no fluxo principal.

--- api.py
+++ api.py
@@ -18,6 +18,17 @@ class Servers:
             "Rounds":[],
             "GameMode":0
         };
+    def get_status(self,key:str):
+        try:
+            if self.get(key,"Online") == False:
+                return "Offline";
+            elif self.get(key,"Online"):
+                if self.o_keys[key]["Players"]["Nmb"] < self.o_keys[key]["Players"]["Max"]:
+                    return "Connected";
+                else:
+                    return "NoConnected";
+        except:
+            return "NoParty";
     def go_server(self,key:str):
         if self.o_keys[key]["Online"] == False:
             self.o_keys[key]["Online"] = True;
@@ -35,70 +46,80 @@ class Servers:
         self.o_keys[key]["Players"]["Users"][name] = {
             "Points":0
         };
+    def desconnect_player(self,key:str,name:str):
+        self.o_keys[key]["Players"]["Nmb"] -= 1;
+        del self.o_keys[key]["Players"]["Users"][name];
     def get(self,key:str,_var:str):
         return self.o_keys[key][_var];
     
 servers = Servers();
 
-@app.put("/go_server/{o_key}/{maxplayers}/{name}")
-def add_or_go(o_key:str,maxplayers:int,name:str):
-    servers.add_server(o_key,maxplayers);
-    servers.go_server(o_key);
-    servers.connect_player(o_key,name);
+@app.put("/go_party/{key}/{maxplayers}/{name}")
+def add_or_go(key:str,maxplayers:int,name:str):
+    servers.add_server(key,maxplayers);
+    servers.go_server(key);
+    servers.connect_player(key,name);
 
-@app.put("/{o_key}/connect/{name}")
-def online(o_key:str,name:str):
+@app.put("/{key}/offline")
+def chance_status(key:str):
+    servers.go_server(key);
+
+@app.put("/{key}/connect/{name}")
+def connect(key:str,name:str):
     # Conecta os jogadores
-    if servers.get(o_key,"Online") == False:
-        return {"Status":"Offline"};
-    elif servers.get(o_key,"Online"):
-        if servers.o_keys[o_key]["Players"]["Nmb"] < servers.o_keys[o_key]["Players"]["Max"]:
-            servers.connect_player(o_key,name);
-            return {"Status":"Connected"};
-        else:
-            return {"Status":"NoConnected"};
+    status = servers.get_status(key);
+    if status == "Connected":
+        servers.connect_player(key,name);
+    return {"Status":status};
 
-@app.put("/{o_key}/ready")
-def update_equation(o_key:str):
+@app.put("/{key}/desconnect/{name}")
+def desconnect(key:str,name:str):
+    # Desconecta os jogadores
+    servers.desconnect_player(key,name);
+    if servers.o_keys[key]["Players"]["Ready"] >= servers.o_keys[key]["Players"]["Nmb"]:
+        servers.update_equation(key);
+
+@app.put("/{key}/ready")
+def update_equation(key:str):
     # Retorna se foi dado o Ready
     ready = False;
-    servers.o_keys[o_key]["Players"]["Ready"] += 1;
-    if servers.o_keys[o_key]["Players"]["Ready"] >= servers.o_keys[o_key]["Players"]["Nmb"]:
-        servers.update_equation(o_key);
+    servers.o_keys[key]["Players"]["Ready"] += 1;
+    if servers.o_keys[key]["Players"]["Ready"] >= servers.o_keys[key]["Players"]["Nmb"]:
+        servers.update_equation(key);
         ready = True;
     return {"Ready":ready};
 
-@app.put("/{o_key}/result/{user}/{result}")
-def note_result(o_key:str,user:str,result:int):
+@app.put("/{key}/result/{user}/{result}")
+def note_result(key:str,user:str,result:int):
     # Retorna 0 caso não tenha conseguido acertar ou o nome do jogador
-    if servers.o_keys[o_key]["Players"]["Ready"] != 0: servers.o_keys[o_key]["Players"]["Ready"] = 0;
-    if result == servers.o_keys[o_key]["Rounds"][-1]["Result"] and servers.o_keys[o_key]["Rounds"][-1]["Winner"] == 0:
-        servers.o_keys[o_key]["Rounds"][-1]["Winner"] = user;
-        servers.o_keys[o_key]["Players"]["Users"][user]["Points"] += 1;
-    return {"Winner":servers.o_keys[o_key]["Rounds"][-1]["Winner"]};
+    if servers.o_keys[key]["Players"]["Ready"] != 0: servers.o_keys[key]["Players"]["Ready"] = 0;
+    if result == servers.o_keys[key]["Rounds"][-1]["Result"] and servers.o_keys[key]["Rounds"][-1]["Winner"] == 0:
+        servers.o_keys[key]["Rounds"][-1]["Winner"] = user;
+        servers.o_keys[key]["Players"]["Users"][user]["Points"] += 1;
+    return {"Winner":servers.o_keys[key]["Rounds"][-1]["Winner"]};
 
-@app.get("/{o_key}/get_ready")
-async def get_ready(o_key:str):
+@app.get("/{key}/get_status")
+def get_status(key:str):
+    return {"Status":servers.get_status(key)};
+
+@app.get("/{key}/get_ready")
+async def get_ready(key:str):
     # Retorna se foi dado o Ready
     ready = False;
-    if servers.o_keys[o_key]["Players"]["Ready"] >= servers.o_keys[o_key]["Players"]["Nmb"]:
+    if servers.o_keys[key]["Players"]["Ready"] >= servers.o_keys[key]["Players"]["Nmb"]:
         ready = True;
-    return {"Ready":ready,"Readys":servers.o_keys[o_key]["Players"]["Ready"]};
+    return {"Ready":ready,"Readys":servers.o_keys[key]["Players"]["Ready"]};
 
-@app.get("/{o_key}/get_equation")
-async def get_equation(o_key:str):
+@app.get("/{key}/get_equation")
+async def get_equation(key:str):
     # Retorna a equação
-    return {"Equation":servers.o_keys[o_key]["Rounds"][-1]};
+    return {"Equation":servers.o_keys[key]["Rounds"][-1]};
 
-@app.get("/{o_key}/players")
-async def get_players(o_key:str):
+@app.get("/{key}/players")
+async def get_players(key:str):
     # Retorna tudo sobre os players
-    return {"Names":list(servers.o_keys[o_key]["Players"]["Users"].keys()),"Nmb":servers.o_keys[o_key]["Players"]["Nmb"],"Max":servers.o_keys[o_key]["Players"]["Max"]};
+    return {"Names":list(servers.o_keys[key]["Players"]["Users"].keys()),"Nmb":servers.o_keys[key]["Players"]["Nmb"],"Max":servers.o_keys[key]["Players"]["Max"]};
 
-@app.get("/{o_key}")
-async def main(o_key:str):
-    return servers.o_keys[o_key];
-
-@app.get("/admin")
-async def all_for_one():
-    return servers.o_keys;
+@app.get("/{key}")
+async def main(key:str):
+    return servers.o_keys[key];
--- main.py
+++ main.py
@@ -4,73 +4,131 @@ import time
 import requests as req
 
 def init():
-    print("Iniciado");
+    print("Bem vindo ao Educ. Games feito para estudantes do IFS - Itabaiana");
     global api
     api = input("Digite a url do servidor: ");
+    try:
+        req.get(api+"/");
+    except:
+        input("Servidor não está ativo!!");
+        quit();
 
 def main():
-    match input("1 - Criar um novo servidor;\n2 - Conectar em um servidor;\n3 - Sair;\nOpção: "):
-        case "1":
-            clear();
-            server_code = input("Digite o código do servidor: ");
-            max_players = input("Digite a quantidade máxima de jogadores: ");
-            user_name = input("Digite um Nome de Exibição: ");
-            print("Criando um novo servidor...");
-            req.put(api+f"/go_server/{server_code}/{max_players}/{user_name}");
-            op = True;
-        case "2":
-            clear();
-            server_code = input("Digite o código do servidor: ");
-            user_name = input("Digite um Nome de Exibição: ");
-            print("Conectando ao servidor...");
-            status = req.put(api+f"/{server_code}/connect/{user_name}").json()["Status"];
-            match status:
-                case "Offline":
-                    print("O servidor se encontra Offline ou não existe.");
-                case "Connected":
-                    print("Conectado!");
-                case "NoConnected":
-                    print("Não foi possível se conectar as servidor. Eles está lotado...");
-            op = False;
-        case "3":
-            clear();
-            print("Saindo...");
-            exit();
+    while True:
+        match input("1 - Criar uma nova Party;\n2 - Conectar em uma Party;\n3 - Sair;\nOpção: "):
+            case "1":
+                clear();
+                server_code = input("Digite o código da Party: ");
+                if req.get(api+f"/{server_code}/get_status").json()["Status"] != "NoParty":
+                    input("Essa Party já está sendo usada!!");
+                    continue;
+                max_players = input("Digite a quantidade máxima de jogadores: ");
+                user_name = input("Digite um Nome de Exibição: ");
+                print("Criando uma nova Party...");
+                req.put(api+f"/go_party/{server_code}/{max_players}/{user_name}");
+                print("Party Criada!!");
+                op = True;
+                break;
+            case "2":
+                clear();
+                server_code = input("Digite o código da Party: ");
+                user_name = input("Digite um Nome de Exibição: ");
+                print("Conectando à Party...");
+                status = req.put(api+f"/{server_code}/connect/{user_name}").json()["Status"];
+                op = False;
+                match status:
+                    case "Offline":
+                        print("A Party se encontra Offline!!");
+                    case "Connected":
+                        print("Conectado!");
+                        break;
+                    case "NoConnected":
+                        print("Não foi possível se conectar a Party. Ela está lotada...");
+                    case "NoParty":
+                        print("A Party não existe...");
+                    case _:
+                        print("Erro Inesperado");
+            case "3":
+                clear();
+                print("Saindo...");
+                exit();
+        input();
     clear();
     players = req.get(api+f"/{server_code}/players").json();
     while players["Nmb"] < players["Max"]:
-        print(f"Jogadores: {players["Nmb"]}/{players["Max"]}\nNomes: {players["Names"]}");
+        print(f"Jogadores: {players["Nmb"]}/{players["Max"]}\nNomes: {players["Names"]}\nCódigo de Party: {server_code}");
         time.sleep(8);
         players = req.get(api+f"/{server_code}/players").json();
         clear();
     print(f"Jogadores: {players["Nmb"]}/{players["Max"]}\nNomes: {players["Names"]}");
     input("Pronto? Pressione Enter para continuar...");
-    ready = req.put(api+f"/{server_code}/ready").json()["Ready"];
-    while ready == False:
+    ready = req.put(api+f"/{server_code}/ready").json();
+    while not ready["Ready"]:
         clear();
-        if ready:
+        if ready["Ready"]:
             break;
-        _ready = req.get(api+f"/{server_code}/get_ready").json();
-        ready = _ready["Ready"];
-        readys = _ready["Readys"];
-        print(f"Jogadores Prontos: {readys}/{players["Nmb"]}");
+        ready = req.get(api+f"/{server_code}/get_ready").json();
+        print(f"Jogadores Prontos: {ready["Readys"]}/{players["Nmb"]}");
         time.sleep(1.5);
-    equation = req.get(api+f"/{server_code}/get_equation").json()["Equation"];
-    print(equation);
-    winner = 0;
-    while winner == 0:
+    while True:
+        equation = req.get(api+f"/{server_code}/get_equation").json()["Equation"];
+        print(equation);
+        winner = 0;
+        while winner == 0:
+            clear();
+            print(f"Equação: {equation["Numbers"][0]}{equation["Operation"]}{equation["Numbers"][1]}\n");
+            result = input("Qual a resposta? ");
+            winner = req.put(api+f"/{server_code}/result/{user_name}/{result}").json()["Winner"];
+            if winner == 0:
+                input("Você errou. Pressione Enter para continuar...");
+        if winner == user_name:
+            print("VOCÊ foi o vencedor!!!");
+        elif winner != user_name:
+            print(f"O vencedor foi {winner}...");
+        input();
         clear();
-        print(f"Equação: {equation["Numbers"][0]}{equation["Operation"]}{equation["Numbers"][1]}\n");
-        result = input("Qual a resposta? ");
-        winner = req.put(api+f"/{server_code}/result/{user_name}/{int(result)}").json()["Winner"];
-        if winner == 0:
-            input("Você errou. Pressione Enter para continuar...");
-    if winner == user_name:
-        print("VOCÊ foi o vencedor!!!");
-    elif winner != user_name:
-        print(f"O vencedor foi {winner}...");
-    input();
-
+        while True:
+            if op:
+                match input("1 - Continuar;\n2 - Desligar Party;\nOpção: "):
+                    case "1":
+                        ready = req.put(api+f"/{server_code}/ready").json();
+                        desconnect = False;
+                    case "2":
+                        req.put(api+f"/{server_code}/offline");
+                        req.put(api+f"/{server_code}/desconnect/{user_name}");
+                        desconnect = True;
+                    case _:
+                        input("Opção não disponível!!");
+                        continue;
+            elif not op:
+                match input("1 - Continuar;\n2 - Desconectar da Party\nOpção: "):
+                    case "1":
+                        ready = req.put(api+f"/{server_code}/ready").json();
+                        desconnect = False;
+                    case "2":
+                        req.put(api+f"/{server_code}/desconnect/{user_name}");
+                        desconnect = True;
+                    case _:
+                        input("Opção não disponível!!");
+                        continue;
+            break;
+        if req.get(api+f"/{server_code}/get_status").json()["Status"] == "Offline" and not desconnect:
+            req.put(api+f"/{server_code}/desconnect/{user_name}");
+            input("A Party foi encerrada");
+            desconnect = True;
+        if desconnect:
+            clear();
+            break;
+        players = req.get(api+f"/{server_code}/players").json();
+        while not ready["Ready"]:
+            ready = req.get(api+f"/{server_code}/get_ready").json();
+            if ready["Ready"]:
+                break;
+            print(f"Jogadores Prontos: {ready["Readys"]}/{players["Nmb"]}");
+            time.sleep(1.5);
+            clear();
+    del desconnect;
+                    
 if __name__ == "__main__":
     init();
     while True:
